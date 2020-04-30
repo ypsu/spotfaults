267d1103cfc5481b349d1a2e5f3131659685558a
Balazs Kezes <rlblaster@gmail.com>
2020-04-30 22:51:25 +0200

    make rmprefix's checker more robust

--- rmprefix/check.c
+++ rmprefix/check.c
@@ -4,14 +4,25 @@
 int main(void) {
   for (int i = 0; i < 2; i++) {
     char str[110];
-    if (scanf("%105s", str) != 1) {
+    if (scanf("%105s ", str) != 1) {
       puts("not enough input.");
       return 1;
     }
-    if (strlen(str) > 100) {
+    int len = strlen(str);
+    if (len > 100) {
       puts("input string too long.");
       return 1;
     }
+    for (int i = 0; i < len; i++) {
+      if (str[i] < 'a' || 'z' < str[i]) {
+        puts("input string has invalid characters.");
+        return 1;
+      }
+    }
+  }
+  if (!feof(stdin)) {
+    puts("too much input.");
+    return 1;
   }
   return 0;
 }
